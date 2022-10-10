a1520070e85e0750fd13126b07da620a7e0304cf
romkeernst <114489585+romkeernst@users.noreply.github.com>
2022-10-10 11:24:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,50 @@
-<?php
+<!DOCTYPE HTML>
+<HTML>
+    <head>
+        <style>
+            html {
+                background-color: lightgray;
+                width: 100%;
+                height: 100%;
+            }
+
+            body {
+                background-color: white;
+                width: 50%;
+                height: 100%;
+                margin-left: 10%;
+                margin-right: 10%;
+                align-content: center;
+                border: 1px solid black;
+            }
+
+            s
+
+
+        </style>
+    </head>
+    <body>
+            <div class="smileys">
+                <label>
+                    <input type="radio" name="smiley" value=":)" checked>
+                    <img src="../GYO-systeem/images/VerySda.png" alt="smile" width="100px" height="100px">
+                </label>
+                <label>
+                    <input type="radio" name="smiley" value=":)" checked>
+                    <img src="../GYO-systeem/images/Sad.png" alt="smile" width="100px" height="100px">
+                </label>
+                <label>
+                    <input type="radio" name="smiley" value=":)" checked>
+                    <img src="../GYO-systeem/images/Moderate.png" alt="smile" width="100px" height="100px">
+                </label>
+                <label>
+                    <input type="radio" name="smiley" value=":)" checked>
+                    <img src="../GYO-systeem/images/Happy.png" alt="smile" width="100px" height="100px">
+                </label>
+                <label>
+                    <input type="radio" name="smiley" value=":)" checked>
+                    <img src="../GYO-systeem/images/VeryHappy.png" alt="smile" width="100px" height="100px">
+                </label>
+            </div>
+    </body>
+</HTML>
